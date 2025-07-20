770be51ceb5b88d6c09a39ac49715ddf9ad334a5
Tingting Zhan <tingtingzhan@gmail.com>
2025-07-20 14:20:23 -0400

    tzh

--- R/md_plot.R
+++ R/md_plot.R
@@ -22,22 +22,11 @@ md_plot_ <- function(x, xnm, ...) {
 #' @rdname md_
 #' @examples
 #' library(lattice); Depth = equal.count(quakes$depth, number=8, overlap=.1)
-#' library(consort); data(dispos.data)
 #' list(
-#'  '`trellis`' = xyplot(lat ~ long | Depth, data = quakes),
-#'  '`consort`' = consort_plot(
-#'   data = dispos.data |> subset(subset = !(arm3 %in% 'Trt C')),
-#'   orders = list(c(trialno = 'Population'), c(exclusion = 'Excluded'), c(arm = 'Randomized')),
-#'   side_box = c('exclusion'))
+#'  '`trellis`' = xyplot(lat ~ long | Depth, data = quakes)
 #' ) |> render_(file = 'Vanilla Plot')
 #' 
 #' @export md_.trellis
 #' @export
 md_.trellis <- md_plot_ 
-
-#' @rdname md_
-#' @export md_.consort
-#' @export
-md_.consort <- md_plot_
-
 
